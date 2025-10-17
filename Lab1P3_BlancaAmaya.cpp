355e08688942a7b80d0dbfd09e59ec7c46708a17
blanca-amaya <bcilena10@gmail.con>
2025-10-17 14:49:21 -0600

    Segundo commit

--- Lab1P3_BlancaAmaya.cpp
+++ Lab1P3_BlancaAmaya.cpp
@@ -1,5 +1,15 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!\n";
+    float peso = 0;
+    float altura = 0;
+    float imc = 0;
+    cout << "Ingrese su peso: ";
+    cin >> peso;
+    cout << "Ingrese su altura: ";
+    cin >> altura;
+    float altura_cm = altura * 0.01;
+    imc = (peso/2.2) / (altura_cm* altura_cm);
+    cout << "Su indice de masa corporal es: " << imc << endl;
 }
